e67d628f19a2192a0793c38729697a513f63f5be
JulianBritton13 <46837272+JulianBritton13@users.noreply.github.com>
2020-05-06 10:30:01 +0200

    Added timed spawner script
    spawns objects after a time

---
+++ Unity project/Random project 1/Assets/Scripts/TimedSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSpawn : MonoBehaviour
+{
+    public GameObject spawnee;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    void Start()
+    {
+        // using invokeRepeating as a timer between object spawns
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    // spawns the objects
+    public void SpawnObject()
+    {
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if (stopSpawning)
+        {
+            // stops the timer for object spawning
+            CancelInvoke("SpawnObject");
+        }
+    }
+
+}
